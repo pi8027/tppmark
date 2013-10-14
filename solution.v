219bf529b26c7c9fc3fdf603004d08e5a050bb31
Kazuhiko Sakaguchi <pi8027@gmail.com>
2013-10-15 02:44:11 +0900

    add definitions of the state transition and the invariant candidate

--- solution.v
+++ solution.v
@@ -1,37 +1,43 @@
 Require Import
   Ssreflect.ssreflect Ssreflect.ssrfun Ssreflect.ssrbool Ssreflect.eqtype
-  Ssreflect.ssrnat Ssreflect.seq Ssreflect.fintype Ssreflect.tuple.
+  Ssreflect.ssrnat Ssreflect.seq Ssreflect.fintype
+  MathComp.bigop MathComp.finset MathComp.fingroup MathComp.perm.
 
 Set Implicit Arguments.
+Unset Strict Implicit.
+Import Prenex Implicits.
 
 Section puzzle.
 
-Variable (x y : nat) (Hx : 0 < x) (Hy : 0 < y).
+Variable (x' y' : nat).
+Definition x := x'.+1.
+Definition y := y'.+1.
 
-Definition elemt : finType := ordinal_finType (x * y).
-
-Lemma board_is_nonempty : (x * y).-1 < (x * y).
-Proof.
-  have: (0 < x * y) by rewrite muln_gt0 Hx Hy.
-  by case: (x * y).
-Qed.
+Definition elemt : finType :=
+  prod_finType (ordinal_finType x) (ordinal_finType y).
+Definition board : baseFinGroupType := perm_of_finGroupType elemt.
 
 Definition empty_space : elemt :=
-  @Ordinal (x * y) (x * y).-1 board_is_nonempty.
+  (@Ordinal x x.-1 (leqnn x), @Ordinal y y.-1 (leqnn y)).
 
-Record board := Board {
-  bbody : 'I_x -> 'I_y -> elemt;
-  _ : forall x1 x2 y1 y2, x1 <> x2 \/ y1 <> y2 -> bbody x1 y1 <> bbody x2 y2
-  }.
+Definition puzzle_next (b : board) : {set board} :=
+  let (ex, ey) := b^-1%g empty_space in
+  [set perm_mul b (tperm (ex, ey) npos) |
+    npos : elemt &
+    (npos \in [:: (inord ex.-1, ey); (inord ex.-1, ey);
+                  (ex, inord ey.-1); (ex, inord ey.+1)]) &&
+    (npos != (ex, ey))].
 
 (*
-another definition:
-
-Record board := Board {
-  bbody : y.-tuple(x.-tuple(elemt));
-  _ : forall x, count (@pred1 elemt x) (flatten (map (@tval _ _) bbody)) = 1
-  }.
+Definition puzzle_next' (b : board) : {set board} :=
+  let (ex, ey) := b^-1%g empty_space in
+  [set perm_mul b (tperm (ex, ey) (ex', ey')) |
+    ex' in 'I_x, ey' in 'I_y & odd (ex' + ex) (+) odd (ey' + ey)].
 *)
+
+Definition invariant (b : board) : bool :=
+  let (ex, ey) := b^-1%g empty_space in
+  odd_perm b (+) odd x' (+) odd y' (+) odd ex (+) odd ey.
 
 
 
